ca34f9745f24192fb04f4386888e61dc1990521c
Bruno Melo <bruno.raphael@gmail.com>
2020-01-31 18:06:15 -0300

    Updates

--- assets/js/task.js
+++ assets/js/task.js
@@ -1,10 +1,12 @@
 var earth = null;
 var markers = [];
 var answered = false;
+var randomPos = [0,0];
 
 function openMap(settings, extraOptions) {
     if (earth === null) {
         earth = new window.WE.map('earth_div', settings);
+        randomPos = extraOptions.initial;
     
         window.WE.tileLayer('https://rvsphere.s3.amazonaws.com/venus/{z}/{x}/{y}.png', {
             minZoom: 0,
@@ -20,29 +22,30 @@ function openMap(settings, extraOptions) {
             if (e.latlng !== undefined && !answered ){
                 answered = true;
                 var posClick = [e.latitude, e.longitude];
-                markers.push( window.WE.marker(extraOptions.initial, '/assets/imgs/marker-blue.png').addTo(earth) );
-                markers.push( window.WE.marker(posClick, '/assets/imgs/marker-green.png').addTo(earth) );
-                earth.setView(extraOptions.initial);
-                earth.panTo(posClick);
-                //TODO: ONLY ADVANCE AFTER ANIMATION
+                markers.push( window.WE.marker(randomPos, '../assets/imgs/marker-blue.png').addTo(earth) );
+                markers.push( window.WE.marker(posClick, '../assets/imgs/marker-green.png').addTo(earth) );
+                //earth.setView(extraOptions.initial);
+                earth.panTo(randomPos);
+                
                 extraOptions.onDoubleClick(posClick[0], posClick[1]);
             }
         });
     }
 }
 
-function resetMap(settings){
+function resetMap(settings, extraOptions){
     if( earth === null ) {
         return false;
     }
 
     earth.setView(settings.center);
+    randomPos = extraOptions.initial;
     earth.da.P.m.forEach( (idx) => { 
         var mk = earth.da.P.O[idx]
         mk.element.remove();
         mk.enabled = false; 
         mk.visible = false; 
     } )
-    //markers = [];
+    markers = [];
     answered = false;
 }
